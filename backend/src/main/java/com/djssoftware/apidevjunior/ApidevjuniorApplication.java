44ef29593af9bc37b3ac9cb64b382ee9e0bb6ac8
Divino Joaquim da Silva <divino_j_silva@hotmail.com>
2022-09-15 09:53:28 -0300

    Created Project

---
+++ backend/src/main/java/com/djssoftware/apidevjunior/ApidevjuniorApplication.java
@@ -0,0 +1,13 @@
+package com.djssoftware.apidevjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidevjuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidevjuniorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/djssoftware/apidevjunior/ApidevjuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djssoftware.apidevjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidevjuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
